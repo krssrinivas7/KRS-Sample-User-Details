f9351a8f2e02d665de1371b6e880d66f0115a262
krssrinivas7 <kosanasrinivas257@gmail.com>
2023-08-08 18:22:21 +0530

    Files

---
+++ register.php
@@ -0,0 +1,34 @@
+<?php
+// Database connection parameters
+$host = "localhost";
+$dbUsername = "your_db_username";
+$dbPassword = "your_db_password";
+$dbName = "your_db_name";
+
+// Create a connection to the database
+$conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Retrieve user input from the form
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+// SQL query to insert data into the database
+$query = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+$stmt = $conn->prepare($query);
+$stmt->bind_param("sss", $username, $email, $password);
+
+if ($stmt->execute()) {
+    echo "Registration successful!";
+} else {
+    echo "Error: " . $stmt->error;
+}
+
+$stmt->close();
+$conn->close();
+?>
